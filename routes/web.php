771f92c38c976dd2d9e98df02992b56a74e216f9
Abdulwahab Ashimi <princeadebowale3@gmail.com>
2023-01-03 04:29:27 +0100

    Routing Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PublicController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::get('/', [PublicController::class, 'index']);
 
